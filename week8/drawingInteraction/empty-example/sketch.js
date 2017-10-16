5f40942caabc35208804cfa8ec132776eb985c68
johnmargas <johnm228@gmail.com>
2017-10-16 12:25:58 -0400

    week8

--- week8/drawingInteraction/empty-example/sketch.js
+++ week8/drawingInteraction/empty-example/sketch.js
@@ -1,8 +1,18 @@
 var centerX = 0;
 var centerY = 0;
-var bgColor;
+
+var bgColor, bgImage, bgImage2, rocket, currentBgImage, bgChange, bgChange2;
+
+var hitZoneX = 100;
+var hitZoneY = 100;
 
 var hairRed, hairGreen, hairBlue;
+
+function preload() {
+    bgImage = loadImage("assets/sky.jpg");
+    bgImage2 = loadImage("assets/space.jpg");
+    rocket = loadImage("assets/rocket.png");
+}
 
 function setup() {
     bgColor = color(255, 255, 0);
@@ -12,16 +22,32 @@ function setup() {
     centerX = width / 2;
     centerY = height / 2;
     
+    var redText = createP("Red");
     hairRed = createSlider(0, 255, 0);
+    var greenText = createP("Green");
     hairGreen = createSlider(0, 255, 0);
+    var blueText = createP("Blue");
     hairBlue = createSlider(0, 255, 0);
+    
+    bgChange = createButton("space");
+    bgChange.position(100, 500);
+    bgChange.mousePressed(changeBgFunction);
+    bgChange2 = createButton("sky");
+    bgChange2.position(300, 500);
+    bgChange2.mousePressed(changeBgFunction2);
+    
+    currentBgImage = bgImage;
 }
 
 function draw() {
+    //console.log(mouseX + ", " + mouseY);
+    
     background(bgColor);
     
-    //centerX = mouseX;
-    //centerY = mouseY;
+    image(currentBgImage, 0, 0);
+    
+    centerX = mouseX;
+    centerY = mouseY;
 
     fill(255)
     stroke(0);
@@ -47,8 +73,28 @@ function draw() {
     arc(centerX, centerY, 100, 100, 0, PI);
     strokeWeight(30);
     arc(centerX, centerY, 100, 100, PI, TWO_PI);
+    
+    stroke("white");
+    ellipse(hitZoneX, hitZoneY, 10, 10);
+    /*
+    if(mouseX == hitZoneX && mouseY == hitZoneY)
+        image(rocket, 100, 100);
+    */
+    
+    var hitZoneDist = dist(mouseX, mouseY, hitZoneX, hitZoneY);
+    
+    if(hitZoneDist <= 20)
+        image(rocket, 100, 100);
 }
 
 function mousePressed() {
-    bgColor = color(0, 255, 255);
+    bgColor = color(255, 0, 0);
+}
+
+function changeBgFunction() {
+    currentBgImage = bgImage2;
+}
+
+function changeBgFunction2() {
+    currentBgImage = bgImage;
 }
