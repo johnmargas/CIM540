ccdb7d1f4aa73aedc374285bdc9626e7d84d2ac2
johnmargas <johnm228@gmail.com>
2017-10-25 13:33:53 -0400

    images

--- week8/drawingInteraction/empty-example/sketch.js
+++ week8/drawingInteraction/empty-example/sketch.js
@@ -16,36 +16,36 @@ function preload() {
 
 function setup() {
     bgColor = color(255, 255, 0);
-    
+
     createCanvas(500, 500);
-    
+
     centerX = width / 2;
     centerY = height / 2;
-    
+
     var redText = createP("Red");
     hairRed = createSlider(0, 255, 0);
     var greenText = createP("Green");
     hairGreen = createSlider(0, 255, 0);
     var blueText = createP("Blue");
     hairBlue = createSlider(0, 255, 0);
-    
+
     bgChange = createButton("space");
     bgChange.position(100, 500);
     bgChange.mousePressed(changeBgFunction);
     bgChange2 = createButton("sky");
     bgChange2.position(300, 500);
     bgChange2.mousePressed(changeBgFunction2);
-    
+
     currentBgImage = bgImage;
 }
 
 function draw() {
     //console.log(mouseX + ", " + mouseY);
-    
+
     background(bgColor);
-    
+
     image(currentBgImage, 0, 0);
-    
+
     centerX = mouseX;
     centerY = mouseY;
 
@@ -55,11 +55,11 @@ function draw() {
 
     //face
     ellipse(centerX, centerY, 100, 100);
-    
+
     //nose
     var noseWiggle = map(mouseX, 0, width, -10, 10);
     ellipse(centerX + noseWiggle, centerY + 10, 20, 20);
-    
+
     //eyes
     ellipse(centerX - 10, centerY - 10, 10, 20);
     ellipse(centerX + 10, centerY - 10, 10, 20);
@@ -73,17 +73,17 @@ function draw() {
     arc(centerX, centerY, 100, 100, 0, PI);
     strokeWeight(30);
     arc(centerX, centerY, 100, 100, PI, TWO_PI);
-    
+
     stroke("white");
     ellipse(hitZoneX, hitZoneY, 10, 10);
     /*
     if(mouseX == hitZoneX && mouseY == hitZoneY)
         image(rocket, 100, 100);
     */
-    
+
     var hitZoneDist = dist(mouseX, mouseY, hitZoneX, hitZoneY);
-    
-    if(hitZoneDist <= 20)
+
+    if (hitZoneDist <= 20)
         image(rocket, 100, 100);
 }
 
