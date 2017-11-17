6727a2cf6f3207906329c19a53c62cb8d81040c8
johnmargas <johnm228@gmail.com>
2017-11-17 11:45:08 -0500

    midterm

---
+++ midterm/sketch.js
@@ -0,0 +1,22 @@
+var load, rm;
+
+function preload() {
+    load = loadStrings('test.txt');
+}
+
+function setup() {
+    createCanvas(400, 400);
+    
+    rm = new RiMarkov(5);
+    for(i = 0; i < load.length; i++)
+        rm.loadText(load[i]);
+    console.log(rm.size());
+//    for(i = 0; i <= rm.size(); i++)
+//        tokens = rm.generateTokens(i);
+//    
+//    console.log(tokens);
+//    
+//    wordArray = RiTa.tokenize(sentence);
+//    
+//    console.log(wordArray);
+}
