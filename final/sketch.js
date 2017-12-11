4c0dd0822db71278d2fe6c2e1db196e8e046629a
johnmargas <johnm228@gmail.com>
2017-12-11 10:39:38 -0500

    final5

--- final/sketch.js
+++ final/sketch.js
@@ -1,37 +1,75 @@
-var isIntScene, isExtScene, isNumber,
-formattedScreenplay = []
-sceneHeading = [], 
-sceneLine = [];
+var intScene = [],
+extScene = [],
+sceneBoundary = [],
+sceneContent = [],
+characterArray = [];
 
 function preload() {
-    screenplay = loadStrings('screenplays/masterOfNone.txt');
+    screenplay = loadStrings('screenplays/her.txt');
 }
 
 function setup() {
     noCanvas();
-    format();
     scene();
-}
-
-function format() {
-    for(var i = 0; i < screenplay.length; i++) {
-        isIntScene = screenplay[i].includes('INT.');
-        isExtScene = screenplay[i].includes('EXT.');
-        isNumber = screenplay[i].match(/[0-9]/g);
-
-        if(isNumber == null || 
-            isIntScene == true || 
-            isExtScene == true) {
-            formattedScreenplay.push(screenplay[i]);
-        }
-
-        if(isIntScene == true || isExtScene == true) {
-            sceneHeading.push(formattedScreenplay[i]);
-            sceneLine.push(i);
-        }
-    }
+    character();
 }
 
 function scene() {
+    for(var i = 0; i < screenplay.length; i++) {
+        var isIntScene = screenplay[i].includes('INT.'),
+        isExtScene = screenplay[i].includes('EXT.');
+
+        if(isIntScene) {
+            intScene.push(screenplay[i]);
+        } else if(isExtScene) {
+            extScene.push(screenplay[i]);
+        }
+
+        if(isIntScene || isExtScene) {
+            sceneBoundary.push([i, 0]);
+        }
+    }
+
+    for(var i = 0; i < sceneBoundary.length; i++) {
+        if(i == sceneBoundary.length - 1) {
+            sceneBoundary[i][1] = screenplay.length - 1;
+        } else {
+            sceneBoundary[i][1] = sceneBoundary[i + 1][0] - 1;            
+        }
+
+        var sceneLine = screenplay.slice(sceneBoundary[i][0], 
+            sceneBoundary[i][1]);
+        sceneContent.push(sceneLine);
+    }
+}
+
+function character() {
+    var wordArray = [];
+    for(var i = 0; i < sceneContent.length; i++) {
+        for(var j = 1; j < sceneContent[i].length; j++) {
+            var isAllCaps = /\b[A-Z]{2,}\b/.test(sceneContent[i][j]);
+            var isContinued = sceneContent[i][j].includes('CONTINUED');
+
+            if(isAllCaps && !isContinued) {
+                wordArray.push([i, j, RiTa.tokenize(sceneContent[i][j])]);
+            }
+        }
+    }
     
+    var counter = 0;
+    for(var i = 0; i < wordArray.length; i++) {
+        for(var j = 0; j < wordArray[i][2].length; j++) {
+            var isAllCaps = /\b[A-Z]{2,}\b/.test(wordArray[i][2][j]);
+            
+            if(isAllCaps) {
+                counter++;
+            }
+        }
+
+        var characterLine = sceneContent[wordArray[i][0]][wordArray[i][1]]
+        if(counter == wordArray[i][2].length && !characterArray.includes(characterLine)) {
+            characterArray.push(characterLine);
+        }
+        counter = 0;
+    }
 }
